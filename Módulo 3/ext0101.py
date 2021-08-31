adab966039d49ff32e2809a96e450c435859b4db
Igor M. B <nicklink.lv@gmail.com>
2021-08-30 21:24:11 -0300

    up

--- Módulo 3/ext0101.py
+++ Módulo 3/ext0101.py
@@ -5,12 +5,38 @@
 # b) de 10 até 0, de 2 em 2
 # c) uma contagem personalizada
 
-
 from time import sleep
 
+def contagem1():
+    for c in range(1, 11):
+        print(c, end=' | ', flush=True)
+        sleep(0.5)
+    print()
+    print('='*45)
+    for h in range(10, -2, -2):
+        print(h, end=' | ', flush=True)
+        sleep(0.5)
+    print()
 
-for c in range(1 , 10):
-    print(f'{c}', end=' > ')
-    
+contagem1()
+inicial = int(input('De: '))
+final = int(input('Até: '))
+jumpers = int(input('Pulando de: '))
 
+def contagem(i, f, j):
+    s = 0
+    if j == 0:
+        j = 1
+    if i > f:
+        j = -j
+    elif i == f:
+        print(f'\033[31mO valor inicial é {i} igual ao final {f}')
+    for v in range(i, f+1, j):
+        print(f'{v:^3}', end=' | ', flush=True)
+        sleep(0.5)
+        s += 1
+        if s >= 3:
+            print()
+            s -= 3
 
+contagem(inicial, final, jumpers)
